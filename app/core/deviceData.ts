3abc6d729f59a2d2d4482430e45cdb5fae326967
drykov-ttc <drycov@gmail.com>
2023-08-14 03:58:29 +0600

    s

--- app/core/deviceData.ts
+++ app/core/deviceData.ts
@@ -557,7 +557,8 @@ type OidLoaderType = {
                                     `dgs36xx_ses32xx_dgs_30xx_ddm_rx_power` :
                                     model.includes("SG200-26") ? `cisco_DDM_S200` : "";
                                     // console.log(util.format("oidSuffix:%s",oidSuffix));
-                    if (oidSuffix==="") {
+                    // if (!oidLoader.hasOwnProperty(oidSuffix)||oidSuffix==="") {
+                        if (oidSuffix==="") {
                         results.push(`${symbols.WarnEmo} Функция DDM не поддерживается или не реализована\n\n`);
                         message += `"error":"ddm not supported"}`;
                         console.error(message);
