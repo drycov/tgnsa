2712ff217a615407dbecaaec2973f125c1d76f0e
drykov-ttc <drycov@gmail.com>
2023-08-14 02:07:39 +0600

    add console log

--- app/utils/snmpFunctions.ts
+++ app/utils/snmpFunctions.ts
@@ -1,18 +1,25 @@
 import { Session } from 'snmp-native';
-import joid from '../../src/oid.json'
+import util from 'util';
+import joid from '../../src/oid.json';
+import helperFunctions from './helperFunctions';
 import messagesFunctions from './messagesFunctions';
 const snmpFunctions = {
     getSingleOID: async (host: string, oid: string, community: string): Promise<any> => {
+        const currentDate = helperFunctions.getHumanDate(new Date());
+        let action = "";
+        let message = util.format('{"date":"%s", "action":"%s", ',currentDate,action='getSingleOID')
         const session = new Session({
             host: host,
             community: community
         });
-
+         message += util.format('"%s":"%s", ',"host",host)
         return new Promise((resolve, reject) => {
             session.get({ oid: oid }, (error, varbinds) => {
                 if (!error) {
                     resolve(varbinds[0].value);
                 } else {
+                    message += util.format('"%s":"%s"}',"error",error)
+                    console.error(message);
                     reject(false);
                 }
             });
@@ -20,6 +27,10 @@ const snmpFunctions = {
     },
     checkSNMP: async (host: string, communities: any): Promise<any> => {
         return new Promise(async (resolve) => {
+            const currentDate = helperFunctions.getHumanDate(new Date());
+            let action = "";
+            let message = util.format('{"date":"%s", "action":"%s", ',currentDate,action='checkSNMP')
+            message += util.format('"%s":"%s", ',"host",host)
             // Recursive function to try different communities
             async function tryCommunities(index: any) {
                 if (index >= communities.length) {
@@ -33,6 +44,8 @@ const snmpFunctions = {
                     const session = await snmpFunctions.getSingleOID(host, oid, community);
 
                     if (session) {
+                        message += util.format('"%s":"%s"}',"status",true)
+                        console.info(message);
                         resolve(community);
                     } else {
                         // Try the next community
@@ -40,32 +53,42 @@ const snmpFunctions = {
                     }
                 } catch (error) {
                     // Handle the error here, if needed
-                    console.error('Error:', error);
+                    message += util.format('"%s":"%s"}',"error",error)
+                    console.error(message);
+                    
                     // Try the next community
                     tryCommunities(index + 1);
                 }
             }
-
             try {
                 await tryCommunities(0); // Start trying communities from index 0
             } catch (error) {
                 messagesFunctions.msgSNMPError(host)
-                console.error('Error in tryCommunities:', error);
+                message += util.format('"%s":"%s"}',"error",error)
+                console.error(message);
                 // Handle the error here, if needed
             }
         });
     },
     getMultiOID: (host: string, oid: string, community: string): Promise<any> => {
+        const currentDate = helperFunctions.getHumanDate(new Date());
+        let action = "";
+        let message = util.format('{"date":"%s", "action":"%s", ',currentDate,action='getMultiOID')
+        message += util.format('"%s":"%s", ',"host",host)
         const session = new Session({
             host: host,
             community: community
         });
+        message += util.format('"%s":"%s", ',"host",host)
+
         const result: any = [];
         return new Promise((resolve, reject) => {
             session.getSubtree({
                 oid: oid
             }, (error, varbinds) => {
                 if (error) {
+                    message += util.format('"%s":"%s"}',"error",error)
+                    console.error(message);
                     reject(true)
                 } else {
                     varbinds.forEach((vb) => {
