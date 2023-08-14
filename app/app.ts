76b10f5bbf2f7a54f606980ef746d9bd127ea1f4
drykov-ttc <drycov@gmail.com>
2023-08-14 07:42:48 +0600

    test

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 import { ConversationFlavor, conversations, createConversation } from "@grammyjs/conversations";
 import { hydrateFiles } from '@grammyjs/files';
 import { FileAdapter } from '@grammyjs/storage-file';
-import { Bot, Context, session } from 'grammy';
+import { Bot, Context, GrammyError, HttpError, session } from 'grammy';
 import helperFunctions from './utils/helperFunctions';
 
 import mainComands from './commands/mainCommands'
@@ -170,4 +170,16 @@ bot.hears(labels.DDMInfoLabel, async (ctx) => {
 
     await ctx.conversation.enter("ddmInfo");
 })
+bot.catch((err:any) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
+});
 export default bot
--- bot.ts
+++ bot.ts
@@ -1,5 +1,4 @@
-import { GrammyError, HttpError } from 'grammy';
-import app from './app/app'
+import app from './app/app';
 
 import { run } from "@grammyjs/runner";
 import database from "./app/utils/database";
@@ -7,19 +6,6 @@ import database from "./app/utils/database";
 const runner = run(app);
 database();
 
-
-app.catch((err:any) => {
-    const ctx = err.ctx;
-    console.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
-    } else {
-        console.error("Unknown error:", e);
-    }
-});
 
 const stopRunner = () => {
     if (runner.isRunning()) {
