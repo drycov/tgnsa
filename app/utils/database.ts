3b867208afe137f90c7d7181cb6bfc209284fd04
drykov-ttc <drycov@gmail.com>
2023-08-20 02:04:54 +0600

    fix

--- app/utils/database.ts
+++ app/utils/database.ts
@@ -8,7 +8,7 @@ const currentDate = helperFunctions.getHumanDate(new Date());
 
 const connectToDatabase = () => {
     let action = connectToDatabase.name;
-    let message = util.format('{"date":"%s", "%s":%s",',currentDate,"action",action)
+    let message = util.format('{"date":"%s", "%s":"%s",',currentDate,"action",action)
     try {
         mongoose.connect(config.mongoURI, config.mongoOptions);
         message += util.format('"%s":"%s"}',"status","DB connect")
