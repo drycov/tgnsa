228da48a0feda764f5c4b07a1f31232d199f12d9
drycov <drycov@gmail.com>
2023-12-13 00:29:42 +0600

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       watch: true,
       autorestart: true,
       max_restarts: 10,
-      ignore_watch: ["logs", "node_modules", "sessions", ".vscode"],
+      ignore_watch: ["logs", "node_modules", "sessions", ".vscode", "source"],
       env_production: {
         NODE_ENV: "production",
         APP_TYPE: "PROD",
